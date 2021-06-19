c7e9093327526cee7b5fbfe2394716beffb643f2
Vincent DAVENEL <vincent@davenel.com>
2021-06-19 11:55:51 +0200

    Initial commit

---
+++ app/Models/Groupe.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Groupe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function agents()
+    {
+        return $this->belongsToMany(Agent::class);
+    }
+}
